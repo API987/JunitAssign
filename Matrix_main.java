deaa97fc4e55f8d2a527f145c42829e4e5a562b8
Ketaki <ketaki.gokhale@apisero.com>
2021-02-24 15:41:02 +0530

    java file changed

--- Matrix_main.java
+++ Matrix_main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Matrix {
+public class Matrix_main {
 	public void MatMult(int A[][], int B[][], int ra, int ca, int rb, int cb) {
 		int[][] C = new int[ra][ca];
 		for (int i = 0; i < ra; i++) {
@@ -40,28 +40,28 @@ public class Matrix {
 	public static void main(String[] args) {
 		Scanner cin = new Scanner(System.in);
 		int ra, ca, rb, cb;
-		System.out.println("Enter rows and columns of  matrix A");
+		System.out.println("Enter rows and columns of  Matrix A");
 		ra = cin.nextInt();
 		ca = cin.nextInt();
 		int[][] A = new int[ra][ca];
-		System.out.println("Enter matrix A row by row");
+		System.out.println("Enter Matrix A row by row");
 		for (int i = 0; i < ra; i++) {
 			for (int j = 0; j < ca; j++) {
 				A[i][j] = cin.nextInt();
 			}
 		}
-		System.out.println("Enter rows and columns of  matrix b");
+		System.out.println("Enter rows and columns of  Matrix b");
 		rb = cin.nextInt();
 		cb = cin.nextInt();
 		int[][] B = new int[rb][cb];
-		System.out.println("Enter matrix B row by row");
+		System.out.println("Enter Matrix B row by row");
 		for (int i = 0; i < rb; i++) {
 			for (int j = 0; j < cb; j++) {
 				B[i][j] = cin.nextInt();
 			}
 		}
 		int c = -1;
-		Matrix m = new Matrix();
+		Matrix_main m = new Matrix_main();
 		do {
 			System.out.println("Enter choice: 0.Exit 1.Multiply");
 			c = cin.nextInt();
@@ -70,9 +70,10 @@ public class Matrix {
 				if (ca == rb)
 					m.MatMult(A, B, ra, ca, rb, cb);
 				else
-					System.out.println("Invalid matrix size");
+					System.out.println("Invalid Matrix size");
 				break;
 			}
 		} while (c != 0);
 	}
 }
+
